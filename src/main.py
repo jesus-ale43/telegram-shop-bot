199ca8f4182233ff19c22f4c1f298e0e26ccc618
Jesús Alejandro (jesus_ale43) <jesusmolinarodrigue26@gmail.com>
2024-10-17 18:20:16 -0300

    fix: logger format typo

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ from handlers.commands import *
 
 
 logging.basicConfig(
-    format="(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 
